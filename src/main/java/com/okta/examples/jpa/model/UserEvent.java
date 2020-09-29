b017b3e9dc774b2203bb5a14baa6bbe09b5e462f
Plàcid Masvidal <placidmasvidal@pop-os.localdomain>
2020-09-29 12:05:05 +0200

    pom dependencies fixed

--- src/main/java/com/okta/examples/jpa/model/UserEvent.java
+++ src/main/java/com/okta/examples/jpa/model/UserEvent.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
